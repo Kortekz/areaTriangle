918cc21cd4b09c14204af8d90acce32fa121cc93
Kortekz <cornebalie789@gmail.com>
2023-11-22 09:40:56 +0200

    response

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function area(){
     let input = document.querySelector('[data-height]').value
     let input2 = document.querySelector('[data-base]').value
     let area = eval (`${divide} * ${input2} * ${input}`)
-    display.value = area
+    display.value = area.toFixed(2)
 }
