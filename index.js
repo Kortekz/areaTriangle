32b16d002ed7df261e5ea84462a28726183a970a
Kortekz <cornebalie789@gmail.com>
2023-11-22 09:37:11 +0200

    CODE

---
+++ index.js
@@ -0,0 +1,15 @@
+// Area of triangle
+
+// formula = A = 1/2 × b × h.
+
+let display = document.querySelector('[data-display]')
+let btn = document.querySelector('[data-btn]')
+btn.addEventListener('click', area)
+let divide = 0.5
+
+function area(){
+    let input = document.querySelector('[data-height]').value
+    let input2 = document.querySelector('[data-base]').value
+    let area = eval (`${divide} * ${input2} * ${input}`)
+    display.value = area
+}
