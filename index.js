c6a0fd724e5f41a91f0e5ee897d8c4fa18cf2489
Kortekz <cornebalie789@gmail.com>
2023-11-22 09:45:55 +0200

    response

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ function area(){
     let area = eval (`${divide} * ${input2} * ${input}`)
     display.value = area.toFixed(2)
 }
+// makes the input display read only
+document.querySelector('[data-display]').readOnly = true
