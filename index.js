ea4ca8260edda0ce21f292ad3ce729e9315fa452
Kortekz <cornebalie789@gmail.com>
2023-11-22 10:22:55 +0200

    Final

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 // formula = A = 1/2 × b × h.
 
+// Declaring the variables
 let display = document.querySelector('[data-display]')
 let btn = document.querySelector('[data-btn]')
 btn.addEventListener('click', area)
 let divide = 0.5
 
+// funtion that calculates the area
 function area(){
     let input = document.querySelector('[data-height]').value
     let input2 = document.querySelector('[data-base]').value
